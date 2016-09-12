8788ea6af92f125f69f83cd68a240049057ca3d4
Hazim <hazim@aurorasolutions.io>
2016-09-12 18:51:02 +0500

    Add vpc peering connection variable

--- modules/vpc-peering/peering-target/variables.tf
+++ modules/vpc-peering/peering-target/variables.tf
@@ -5,3 +5,7 @@ variable "target_route_table_ids" {
 variable "root_vpc_cidr" {
   description = "VPC CIDR of the root VPC"
 }
+
+variable "vpc_peering_connection_id" {
+  default = "ID of the peering connection"
+}
