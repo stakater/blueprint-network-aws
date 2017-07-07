4965756e91685ca697ad78adfc0fa58bbdd61b0b
hazim1093 <hazim_malik@hotmail.com>
2017-07-07 14:49:08 +0500

    Update variable interpolation

--- modules/vpc-peering/main.tf
+++ modules/vpc-peering/main.tf
@@ -11,14 +11,14 @@ resource "aws_vpc_peering_connection" "vpc_peering_connection" {
 }
 
 resource "aws_route" "root_to_peer_private_route" {
-  count = "${length(var.peer_vpc_id) > 0 ? ${var.root_route_table_ids_count} : 0}"
+  count = "${length(var.peer_vpc_id) > 0 ? var.root_route_table_ids_count : 0}"
   route_table_id            = "${element(split(",", var.root_route_table_ids), count.index)}"
   destination_cidr_block    = "${var.peer_vpc_cidr}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.vpc_peering_connection.id}"
 }
 
 resource "aws_route" "peer_to_root_private_route" {
-  count = "${length(var.peer_vpc_id) > 0 ? ${var.root_route_table_ids_count} : 0}"                          # https://github.com/hashicorp/terraform/issues/3888"
+  count = "${length(var.peer_vpc_id) > 0 ? var.root_route_table_ids_count : 0}"                          # https://github.com/hashicorp/terraform/issues/3888"
   route_table_id            = "${element(split(",", var.peer_route_table_ids), count.index)}"
   destination_cidr_block    = "${var.root_vpc_cidr}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.vpc_peering_connection.id}"
