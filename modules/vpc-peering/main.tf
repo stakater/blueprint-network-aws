f2b3793badd75e2c7d1e11baeaeee2818cd1f459
hazim1093 <hazim_malik@hotmail.com>
2017-07-07 12:46:20 +0500

    Add count to peering connection

--- modules/vpc-peering/main.tf
+++ modules/vpc-peering/main.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_peering_connection" "vpc_peering_connection" {
+  count = "${length(var.peer_vpc_id) > 0 ? 1 : 0}"
   peer_owner_id = "${var.peer_owner_id}"
   peer_vpc_id   = "${var.peer_vpc_id}"
   vpc_id        = "${var.root_vpc_id}"
